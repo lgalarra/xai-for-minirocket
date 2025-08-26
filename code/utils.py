685518e7a25f480908c2b44949df71e736bfe7aa
lgalarra <luis.galarraga@inria.fr>
2025-08-26 18:23:43 +0200

    Cognitive circles data + training examples

--- code/utils.py
+++ code/utils.py
@@ -1,25 +1,33 @@
 import numpy as np
+import pandas as pd
 from scipy.spatial.distance import cdist
 
+def medoid(X: pd.DataFrame):
+    """
+    Return the index label of the medoid of a pandas DataFrame X (rows = samples).
+    """
+    X_array = X.to_numpy()
+    distances = cdist(X_array, X_array, metric='euclidean')
+    total_distances = distances.sum(axis=1)
+    medoid_position = total_distances.argmin()
+    return X.index[medoid_position]
+
 def medoid_per_class(X, y):
-    X = np.array(X)  # ensure numpy array
-    y = np.array(y)
     medoids = {}
 
     for label in np.unique(y):
         # Extract samples of this class
         X_class = X[y == label]
 
-        # Compute distance matrix
-        distances = cdist(X_class, X_class, metric='euclidean')
-
-        # Sum distances for each point
-        total_distances = distances.sum(axis=1)
-
         # Index of the minimum sum distance → medoid
-        medoid_idx = np.argmin(total_distances)
+        medoid_idx = medoid(X_class)
 
         # Store medoid vector
-        medoids[label] = X_class[medoid_idx]
+        medoids[label] = medoid_idx
 
     return medoids
+
+def export(s, columns, suffix, folder='data'):
+    N = int(len(s.index) / len(columns))
+    for (VAR, VARNAME) in columns:
+        s[[VAR  + str(i) for i in range(1, N+1)]].to_frame().to_csv(f'{folder}/{VARNAME}/{VARNAME}_{suffix}.csv', header=False)
